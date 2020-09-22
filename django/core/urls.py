4137280e418b29d291cb6eb35104ae483bd18a63
veryacademy <35196453+veryacademy@users.noreply.github.com>
2020-09-22 17:02:46 +0100

    nc

---
+++ django/core/urls.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # API Token Management
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Project URLs
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    # User Management
+    path('api/user/', include('users.urls', namespace='users')),
+    # Blog_API Application
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+
+    # API schema and Documentation
+    path('project/docs/', include_docs_urls(title='BlogAPI')),
+    path('project/schema', get_schema_view(
+        title="BlogAPI",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
